06f26adf7941a1d13b76be62c0777b97d2405026
Florin Braghis <florin.braghis@gmail.com>
2025-05-26 19:07:38 +0200

    Remove comment

--- test/xitdb/database_test.clj
+++ test/xitdb/database_test.clj
@@ -3,11 +3,6 @@
     [clojure.test :refer :all]
     [xitdb.test-utils :as tu :refer [with-db]]))
 
-(comment
-  (let [db (tu/test-memory-db-raw)]
-    (reset! db {:foo :bar})
-    @db))
-
 (deftest DatabaseTest
   (with-db [db (tu/test-db)]
     (testing "Resetting to map"
