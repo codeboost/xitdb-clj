cc1b105c2df708e74e42d70a482f1a4efee1873f
Florin Braghis <florin.braghis@gmail.com>
2025-06-02 08:20:26 +0200

    Deploy to clojars

---
+++ build.clj
@@ -0,0 +1,36 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'io.github.codeboost/xitdb-clj)
+(def version "0.1.1")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     basis
+                :src-dirs  ["src"]
+                :scm       {:url "https://github.com/codeboost/xitdb-clj"}
+                :pom-data  [[:description "Embeddable, immutable database."]
+                            [:url "https://github.com/codeboost/xitdb-clj"]
+                            [:licenses
+                             [:license
+                              [:name "MIT License"]
+                              [:url "https://opensource.org/licenses/MIT"]]]]})
+  (b/copy-dir {:src-dirs   ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file  jar-file}))
+
+(defn deploy [_]
+  (jar nil)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+   {:installer :remote
+    :artifact  jar-file
+    :pom-file  (b/pom-path {:lib lib :class-dir class-dir})}))
