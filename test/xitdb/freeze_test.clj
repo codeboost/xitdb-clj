36b99d37b9e25c695651be9b85a44244fb5c6850
radar roark <122068506+radarroark@users.noreply.github.com>
2026-01-15 21:37:10 -0500

    add more to freeze tests

--- test/xitdb/freeze_test.clj
+++ test/xitdb/freeze_test.clj
@@ -32,6 +32,20 @@
 
       (is (= {:fruits ["apple" "pear" "grape"]
               :food ["apple" "pear" "grape" "eggs" "rice" "fish"]}
+             (xdb/materialize @db)))))
+
+  (testing "with freeze and modifying return value"
+    (with-open [db (xdb/xit-db :memory)]
+      (reset! db {})
+
+      (swap! db (fn [moment]
+                  (let [moment (assoc moment :fruits ["apple" "pear" "grape"])
+                        moment (assoc moment :food (conj (xdb/freeze! (:fruits moment))
+                                                         "eggs" "rice" "fish"))]
+                    moment)))
+
+      (is (= {:fruits ["apple" "pear" "grape"]
+              :food ["apple" "pear" "grape" "eggs" "rice" "fish"]}
              (xdb/materialize @db))))))
 
 (deftest freeze-linked-array-list-test
@@ -57,6 +71,20 @@
                   (let [moment (assoc moment :fruits '("apple" "pear" "grape"))
                         moment (assoc moment :food (xdb/freeze! (:fruits moment)))
                         moment (update moment :food conj "eggs" "rice" "fish")]
+                    moment)))
+
+      (is (= {:fruits ["apple" "pear" "grape"]
+              :food ["fish" "rice" "eggs" "apple" "pear" "grape"]}
+             (xdb/materialize @db)))))
+
+  (testing "with freeze and modifying return value"
+    (with-open [db (xdb/xit-db :memory)]
+      (reset! db {})
+
+      (swap! db (fn [moment]
+                  (let [moment (assoc moment :fruits '("apple" "pear" "grape"))
+                        moment (assoc moment :food (conj (xdb/freeze! (:fruits moment))
+                                                         "eggs" "rice" "fish"))]
                     moment)))
 
       (is (= {:fruits ["apple" "pear" "grape"]
@@ -90,6 +118,21 @@
 
       (is (= {:fruits {:names ["apple" "pear" "grape"]}
               :food {:names ["apple" "pear" "grape" "eggs" "rice" "fish"]}}
+             (xdb/materialize @db)))))
+
+  (testing "with freeze and modifying return value"
+    (with-open [db (xdb/xit-db :memory)]
+      (reset! db {})
+
+      (swap! db (fn [moment]
+                  (let [moment (assoc moment :fruits {:names ["apple" "pear" "grape"]})
+                        moment (assoc moment :food (update (xdb/freeze! (:fruits moment))
+                                                           :names conj
+                                                           "eggs" "rice" "fish"))]
+                    moment)))
+
+      (is (= {:fruits {:names ["apple" "pear" "grape"]}
+              :food {:names ["apple" "pear" "grape" "eggs" "rice" "fish"]}}
              (xdb/materialize @db))))))
 
 (deftest freeze-hash-set-test
@@ -115,6 +158,20 @@
                   (let [moment (assoc moment :fruits #{"apple" "pear" "grape"})
                         moment (assoc moment :food (xdb/freeze! (:fruits moment)))
                         moment (update moment :food conj "eggs" "rice" "fish")]
+                    moment)))
+
+      (is (= {:fruits #{"apple" "pear" "grape"}
+              :food #{"apple" "pear" "grape" "eggs" "rice" "fish"}}
+             (xdb/materialize @db)))))
+
+  (testing "with freeze and modifying return value"
+    (with-open [db (xdb/xit-db :memory)]
+      (reset! db {})
+
+      (swap! db (fn [moment]
+                  (let [moment (assoc moment :fruits #{"apple" "pear" "grape"})
+                        moment (assoc moment :food (conj (xdb/freeze! (:fruits moment))
+                                                         "eggs" "rice" "fish"))]
                     moment)))
 
       (is (= {:fruits #{"apple" "pear" "grape"}
